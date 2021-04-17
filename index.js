58c2a5862926fd86baa244f34474500d418864fb
Dennis Pavlyuk <37779084+dpsoccerdude101@users.noreply.github.com>
2021-04-17 17:19:38 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 (() => {
   const nvidiaModels = ["6429440", "6432400"];
+  const elem = document.querySelector(".fade-in-text");
   let log = 0;
   const queryAPI = async (model) => {
     log = log == 0 ? 1 : 0;
     const response = await fetch(
-      `https://us-central1-kyle-s-3080.cloudfunctions.net/api/findInStock/`,
+      "https://us-central1-kyle-s-3080.cloudfunctions.net/api/findInStock/",
       {
-        method: "post",
-        mode: "no-cors",
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Accept: "application/json",
         },
         body: JSON.stringify({ model: model }),
       }
     );
     if (response.ok) {
       const responseJSON = await response.json();
-      console.dir(responseJSON);
-      clearInterval(repeatRun);
+      console.dir(responseJSON.response);
+      if (responseJSON.response == `Hi, the item is not available`)
+        elem.innerText = "No, the item is not in stock yet.";
+      else {
+        clearInterval(repeatRun);
+        elem.innerText = "Yes, the item is in stock!";
+      }
     } else console.log("Error Code: " + response.status);
   };
   const repeatRun = setInterval(() => queryAPI(nvidiaModels[log]), 3000);
